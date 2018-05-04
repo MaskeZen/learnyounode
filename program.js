5975d53da11c8d42642b7b3320bbad48d923b927
Diego Mascheroni <maskeee@gmail.com>
2018-05-04 19:45:45 -0300

    learnyounode_03

--- program.js
+++ program.js
@@ -1,8 +1,20 @@
+// import fs from "fs";
+let fs = require('fs')
+
+// learnyounode_01
 // console.log("HELLO WORLD")
 
-let sum = 0
-for (let i = 2; i < process.argv.length; i++) {
-    sum += Number(process.argv[i])
-}
+// learnyounode_02
+// let sum = 0
+// for (let i = 2; i < process.argv.length; i++) {
+//     sum += Number(process.argv[i])
+// }
 
-console.log(sum)
+// console.log(sum)
+
+// learnyounode_03
+// MY FIRST I/O! (Exercise 3 of 13) 
+let file = process.argv[2]
+let buffer = fs.readFileSync(file)
+let size = buffer.toString().split('\n').length - 1
+console.log(size)
